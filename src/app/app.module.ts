d5e80be1700ce6948f4ce23220f03ac3fd1d3fbb
Santi-crown <Samtigomusik@gmail.com>
2021-12-23 12:12:38 -0500

    Test Succesfull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EjemplosTestComponent } from './ejemplos-test/ejemplos-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjemplosTestComponent
   ],
   imports: [
     BrowserModule
